42cebe1ff9f06f309cd49c1a911b92cd61a977d6
Huy Pham <quanghuy19081@gmail.com>
2024-08-05 08:08:51 +0700

    Initial Commit

---
+++ test_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_1
+//
+//  Created by Huy Pham Quang on 5/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_1/test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  test_1App.swift
+//  test_1
+//
+//  Created by Huy Pham Quang on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
